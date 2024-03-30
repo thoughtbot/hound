6297578aa42af360d5d6fd057762f672891d008c
gujjiomprakash@gmail.com <162006519+OmprakashGujji@users.noreply.github.com>
2024-03-30 19:29:27 +0530

    Add sample code

---
+++ sample_code.py
@@ -0,0 +1,12 @@
+# This is a sample code snippet for receiving suggestions from Hound
+class SampleCode:
+    def __init__(self):
+        self.message = "Initializing SampleCode"
+
+    def sample_method(self):
+        # Please replace this line with your actual code for which you want suggestions
+        print("Replace this line with your code")
+
+# Instantiate the SampleCode class and call the sample_method
+sample_code = SampleCode()
+sample_code.sample_method()
