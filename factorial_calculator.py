d68baf09e3e92ac9b1d02d4f755237f540041396
gujjiomprakash@gmail.com <162006519+OmprakashGujji@users.noreply.github.com>
2024-03-30 10:56:25 +0530

    Add factorial calculator

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,17 +1,10 @@
-class FactorialCalculator:
-    def calculate_factorial(self, n):
-        if n < 0:
-            return None  # Factorial is not defined for negative numbers
-        elif n == 0:
-            return 1  # Factorial of 0 is 1
-        else:
-            result = 1
-            for i in range(1, n + 1):
-                result *= i
-            return result
+# This is a Python script to calculate the factorial of a number
 
-# Usage example:
-calculator = FactorialCalculator()
-number = 5
-result = calculator.calculate_factorial(number)
-print(f"The factorial of {number} is {result}")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = 5
+print(f"The factorial of {num} is {factorial(num)}")
