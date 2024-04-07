5ea876fb808761268a1f43dddfc879464afc1e74
gujjiomprakash@gmail.com <162006519+OmprakashGujji@users.noreply.github.com>
2024-04-07 18:12:20 +0530

    Add numpy_array_example.py file

---
+++ numpy_array_example.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create an array
+arr = np.array([1, 2, 3, 4, 5])
+
+# Print the array
+print("Original Array:", arr)
+
+# Intentional Error: Accessing an index that doesn't exist
+print("Element at index 10:", arr[10])
